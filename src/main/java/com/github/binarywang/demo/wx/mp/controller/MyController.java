d3a7e9dd110215f05391fd65730dd936c1a11015
finnz <finnz@ziztour.com>
2019-07-12 11:08:11 +0800

    新添加的Controller

---
+++ src/main/java/com/github/binarywang/demo/wx/mp/controller/MyController.java
@@ -0,0 +1,14 @@
+package com.github.binarywang.demo.wx.mp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/my")
+public class MyController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "hello";
+    }
+}
